194f6145a2826de9769e59f02982cc70cf529c59
Amin7775 <amin.dpi24@gmail.com>
2024-03-16 14:37:05 +0600

    Completed Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Header from "./components/header/mainHeader/Header";
+
 function App() {
   return (
     <>
-      
+      <div className="container mx-auto">
+        {/* header */}
+        <Header></Header>
+      </div>
     </>
   );
 }
---
+++ src/components/header/mainHeader/Header.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../Nav/Navbar";
+
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className="mb-12">
+            <Navbar></Navbar>
+            </div>
+            <div className="mb-12">
+            
+            </div>
+        </div>
+    );
+};
+
+export default Header;
